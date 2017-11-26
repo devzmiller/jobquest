4a060b413e4e5bb93a08ff3bfd6dd2ba169caf20
Devin Miller <devzmiller@gmail.com>
2017-11-25 16:24:24 -0800

    Add ConnectionsController#create and #show and beginning of show view.

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -4,8 +4,22 @@ class ConnectionsController < ApplicationController
   end
 
   def create
+    @connection = Connection.new(connection_params)
+    if @connection.save
+      redirect_to connection_path(@connection)
+    else
+      @errors = @connection.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @connection = Connection.find(params[:id])
+  end
+
+  private
+
+  def connection_params
+    params.require(:connection).permit(:name, :email, :phone, :company, :job_title, :first_contact, :first_response, :follow_up, :second_response, :coffee_date)
   end
 end
